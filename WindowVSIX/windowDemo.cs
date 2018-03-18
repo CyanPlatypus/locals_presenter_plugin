aaa399606b53390853340a11ea8e5613ae48c636
cyanplatypus <julia.a.andr@gmail.com>
2018-03-18 19:30:09 +0300

    simple visualization added

--- WindowVSIX/windowDemo.cs
+++ WindowVSIX/windowDemo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
@@ -41,7 +42,7 @@ namespace WindowVSIX
             wControl = new windowDemoControl();
             wControl.button1.Click += Button1_Click;
             this.Content = wControl;
-            
+
             dte = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE)) as DTE;
             dte.Events.DebuggerEvents.OnContextChanged +=
                 new _dispDebuggerEvents_OnContextChangedEventHandler(DebuggerEvents_OnContextChanged);
@@ -60,7 +61,7 @@ namespace WindowVSIX
         }
 
         private DTE dte;
-        
+
 
         void DebuggerEvents_OnContextChanged(Process NewProcess, Program NewProgram, Thread NewThread,
             StackFrame NewStackFrame)
@@ -74,56 +75,114 @@ namespace WindowVSIX
 
             //if (locWind == null) return;
             if (dte.Debugger.CurrentStackFrame == null) return;
-            
+
             Expressions generalRegisters = dte.Debugger.CurrentStackFrame.Locals/*.GetExpression("General Registers", true, 1).DataMembers*/;
-            Expressions specialRegisters = dte.Debugger.CurrentStackFrame.Arguments;//GetExpression("Special Registers", true, 1).DataMembers;
+            //Expressions specialRegisters = dte.Debugger.CurrentStackFrame.Arguments;//GetExpression("Special Registers", true, 1).DataMembers;
 
             //ushort[] registers = new ushort[26];
             //string[] registers = new string[26];
 
             StringBuilder stringBuilder = new StringBuilder();
 
-            //int currentRegister = 0;
+            int id = 1;
+
+
+            List<Node> nodesToDraw = new List<Node>();
+            Dictionary<string, Node> hashes = new Dictionary<string, Node>();
+
             foreach (Expression gr in generalRegisters)
             {
-                
-                stringBuilder.Append($"♦ {gr.Name} ({gr.Type}) = {gr.Value} {gr.IsValidValue}\n");
-
-
-                object u = new object();
-                //Object.ReferenceEquals()
-
-                Expression e = dte.Debugger.GetExpression($"{gr.Name}.GetHashCode()");
-                stringBuilder.Append($"[{e.Name}=({e.Type})={e.Value}]\n");
-
-                Expression e2 = dte.Debugger.GetExpression($"LocalPresenter.PresenterHelper.GetAddress({gr.Name})");
-                stringBuilder.Append($"[{e2.Name}=({e2.Type})={e2.Value}]\n");
-
-                foreach (Expression grDataMember in gr.DataMembers)
-                {
-
-
-                    stringBuilder.Append($"\t♥ {grDataMember.Name} ({grDataMember.Type}) =" +
-                                         $" {grDataMember.Value}\n");
-
-                    Expression innerE = dte.Debugger.GetExpression($"{gr.Name}.{grDataMember.Name}.GetHashCode()");
-                    stringBuilder.Append($"\t  [{innerE.Name}=({innerE.Type})={innerE.Value}]\n");
-
-                    Expression e22 = dte.Debugger.GetExpression($"LocalPresenter.PresenterHelper.GetAddress({gr.Name}.{grDataMember.Name})");
-                    stringBuilder.Append($"[{e22.Name}=({e22.Type})={e22.Value}]\n");
-                }
-
-                //foreach (Expression expression in gr.Collection)
-                //{
-                //    stringBuilder.Append($"\t ♦ {expression.Name} ({expression.Type}) =" +
-                //                         $" {expression.Value} {expression.IsValidValue}\n");
-                //}
-
-                ////registers[currentRegister++] = Convert.ToUInt16(gr.Value, 16);
-                //registers[currentRegister++] = gr.Name; //.Value;
-                //registers[currentRegister++] = gr.Type;
-                //registers[currentRegister++] = gr.Value;
+                BuildNode(null, nodesToDraw, hashes, gr, string.Empty, ref id, true);
             }
+
+            //int currentRegister = 0;
+            //foreach (Expression gr in generalRegisters)
+            //{
+
+            //    stringBuilder.Append($"♦ {gr.Name} ({gr.Type}) = {gr.Value} {gr.IsValidValue}\n");
+
+
+            //    object u = new object();
+            //    //Object.ReferenceEquals()
+
+            //    if (dte.Debugger.GetExpression($"{gr.Name}==null").Value == "true")
+            //        continue;
+
+            //    Expression isValueType = dte.Debugger.GetExpression($"{gr.Name}.GetType().IsValueType");
+
+            //    if (isValueType.Value == "true")
+            //        continue;
+
+            //    Expression e = dte.Debugger.GetExpression($"{gr.Name}.GetHashCode()");
+
+            //    string hash = e.Value;
+
+            //    if (!hashes.ContainsKey((hash)))
+            //    {
+            //        Node node = new Node() {Hash = hash, Id = (id++).ToString(), Caption = gr.Name+" "+gr.Type};
+
+            //        hashes.Add(hash, node);
+
+            //        foreach (Expression grDataMember in gr.DataMembers)
+            //        {
+            //            if (dte.Debugger.GetExpression($"{gr.Name}.{grDataMember.Name}==null").Value == "true")
+            //                continue;
+
+            //            isValueType = dte.Debugger.GetExpression($"{gr.Name}.{grDataMember.Name}.GetType().IsValueType");
+
+            //            if (isValueType.Value == "true")
+            //                continue;
+
+            //            e = dte.Debugger.GetExpression($"{gr.Name}.{grDataMember.Name}.GetHashCode()");
+            //            string dataMemberHash = e.Value;
+
+            //            if (!hashes.ContainsKey(dataMemberHash))
+            //            {
+            //                Node datamemberNode = new Node() { Hash = dataMemberHash, Id = (id++).ToString(), Caption = grDataMember.Name };
+            //                hashes.Add(dataMemberHash, datamemberNode);
+            //                node.ChildNodes.Add(datamemberNode);
+            //            }
+
+            //        }
+
+            //        nodesToDraw.Add(node);
+            //    }
+            //    else
+            //    {
+            //        hashes[hash].Caption = gr.Name;
+            //    }
+
+            //    ////Expression e = dte.Debugger.GetExpression($"{gr.Name}.GetHashCode()");
+            //    //stringBuilder.Append($"[{e.Name}=({e.Type})={e.Value}]\n");
+
+            //    //Expression e2 = dte.Debugger.GetExpression($"LocalPresenter.PresenterHelper.GetAddress({gr.Name})");
+            //    //stringBuilder.Append($"[{e2.Name}=({e2.Type})={e2.Value}]\n");
+
+            //    //foreach (Expression grDataMember in gr.DataMembers)
+            //    //{
+
+
+            //    //    stringBuilder.Append($"\t♥ {grDataMember.Name} ({grDataMember.Type}) =" +
+            //    //                         $" {grDataMember.Value}\n");
+
+            //    //    Expression innerE = dte.Debugger.GetExpression($"{gr.Name}.{grDataMember.Name}.GetHashCode()");
+            //    //    stringBuilder.Append($"\t  [{innerE.Name}=({innerE.Type})={innerE.Value}]\n");
+
+            //    //    Expression e22 = dte.Debugger.GetExpression($"LocalPresenter.PresenterHelper.GetAddress({gr.Name}.{grDataMember.Name})");
+            //    //    stringBuilder.Append($"[{e22.Name}=({e22.Type})={e22.Value}]\n");
+            //    //}
+
+            //    ////foreach (Expression expression in gr.Collection)
+            //    ////{
+            //    ////    stringBuilder.Append($"\t ♦ {expression.Name} ({expression.Type}) =" +
+            //    ////                         $" {expression.Value} {expression.IsValidValue}\n");
+            //    ////}
+
+            //    //////registers[currentRegister++] = Convert.ToUInt16(gr.Value, 16);
+            //    ////registers[currentRegister++] = gr.Name; //.Value;
+            //    ////registers[currentRegister++] = gr.Type;
+            //    ////registers[currentRegister++] = gr.Value;
+            //}
 
             //foreach (Expression sr in specialRegisters)
             //{
@@ -148,7 +207,73 @@ namespace WindowVSIX
 
             //this.Caption = string.Join("; ",registers.ToString()); 
             wControl.CleanLog();
-            wControl.AddLog(stringBuilder.ToString());
+            //wControl.AddLog(stringBuilder.ToString());
+            wControl.DrawNodes(nodesToDraw);
         }
+
+        public void BuildNode(Node parent, List<Node> nodeList, Dictionary<string, Node> hashNodeDict, Expression ex
+            , string howToCallExWithDot, ref int idCounter, bool isInLocal = false)
+        {
+            //check for null
+
+            if (dte.Debugger.GetExpression($"{howToCallExWithDot}{ex.Name}==null").Value == "true")
+                return;
+            //check for value type
+            if (dte.Debugger.GetExpression($"{howToCallExWithDot}{ex.Name}.GetType().IsValueType").Value == "true")
+                return;
+
+            string hash = dte.Debugger.GetExpression($"{howToCallExWithDot}{ex.Name}.GetHashCode()").Value;
+
+            if (!hashNodeDict.ContainsKey((hash)))
+            {
+                Node node = new Node()
+                {
+                    Hash = hash,
+                    Id = (idCounter++).ToString(),
+                    Caption = ex.Name + Environment.NewLine +
+                              dte.Debugger.GetExpression($"{howToCallExWithDot}{ex.Name}.GetType().Name").Value
+                };
+
+                hashNodeDict.Add(hash, node);
+
+                if (parent != null && !parent.ChildNodes.Exists(n => n.Hash == hash))
+                    parent.ChildNodes.Add(node);
+                if (parent == null)
+                    nodeList.Add(node);
+
+                foreach (Expression grDataMember in ex.DataMembers)
+                {
+                    BuildNode(node, nodeList, hashNodeDict, grDataMember, 
+                        howToCallExWithDot+ ex.Name + ".",
+                        ref idCounter);
+
+                }
+
+            }
+            else 
+            {
+                Node existingNode = hashNodeDict[hash];
+                if (existingNode != null)
+                {
+                    if (isInLocal)
+                        existingNode.Caption = ex.Name + Environment.NewLine + 
+                            dte.Debugger.GetExpression($"{howToCallExWithDot}{ex.Name}.GetType().Name").Value;
+
+                    if (parent != null && !parent.ChildNodes.Exists(n=>n.Hash == hash))
+                        parent.ChildNodes.Add(existingNode);
+                }
+            }
+        }
+    }
+
+
+
+    public class Node
+    {
+        public string Caption { get; set; }
+        public string Id { get; set; }
+        public string Hash { get; set; }
+
+        public List<Node> ChildNodes { get; set; } = new List<Node>();
     }
 }
--- WindowVSIX/windowDemoControl.xaml.cs
+++ WindowVSIX/windowDemoControl.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace WindowVSIX
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.WpfGraphControl;
+
+namespace WindowVSIX
 {
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
@@ -9,12 +14,26 @@
     /// </summary>
     public partial class windowDemoControl : UserControl
     {
+        DockPanel graphViewerPanel = new DockPanel();
+        //System.Windows.Controls.ToolBar toolBar = new System.Windows.Controls.ToolBar();
+        GraphViewer graphViewer = new GraphViewer();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="windowDemoControl"/> class.
         /// </summary>
         public windowDemoControl()
         {
             this.InitializeComponent();
+
+            graphViewerPanel.ClipToBounds = true;
+            //mainGrid.Height = 300;
+            //mainGrid.Width = 300;
+            mainGrid.Children.Add(graphViewerPanel);
+            graphViewer.BindToPanel(graphViewerPanel);
+
+            //graphViewer.MouseDown += WpfApplicationSample_MouseDown;
+            //this.myGrid.Children.Add(mainGrid);
+
         }
 
         /// <summary>
@@ -39,6 +58,39 @@
         public void CleanLog()
         {
             tbMain.Document.Blocks.Clear();
+        }
+
+        public void DrawNodes(List<Node> nodes)
+        {
+            Graph graph = new Graph();
+
+            nodes.ForEach(n=>DrawNode(n, graph));
+
+            graph.Attr.LayerDirection = LayerDirection.TB;
+            graph.Attr.SimpleStretch = false;
+            graph.Attr.AspectRatio = 1;
+
+            graphViewer.Graph = graph;
+        }
+
+        protected void DrawNode(Node n, Graph graph)
+        {
+            if (n == null) return;
+            //do not draw what already was drawn
+            if (graph.Nodes.Any(nn => nn.Id == n.Id))
+                return;
+
+            Microsoft.Msagl.Drawing.Node
+                newNode = new Microsoft.Msagl.Drawing.Node(n.Id) {LabelText = n.Caption ?? "?"};
+            newNode.Attr.FillColor = Color.LightGray;
+            //font font size
+            //newNode.Label.FontSize = 11;
+            graph.AddNode(newNode);
+            //if (parent != null)
+            //    graph.AddEdge(parent.Id, n.Id);
+
+            n.ChildNodes.ForEach(chN=>DrawNode(chN, graph));
+            n.ChildNodes.ForEach(chN => graph.AddEdge(n.Id, chN.Id));
         }
     }
 }
